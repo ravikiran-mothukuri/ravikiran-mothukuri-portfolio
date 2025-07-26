54ff7fad3e6536ada44a681b09a72b378ccecdaa
ravikiran-mothukuri <ravikiranmothukuri699@gmail.com>
2025-07-26 15:08:25 +0530

    Fix vite base and router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/ravikiran-mothukuri-portfolio/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
